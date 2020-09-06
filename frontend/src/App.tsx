45010cc873b91a8dd54d41bf490054ec1eed2294
ruijadom <ruijadomingues@gmail.com>
2020-09-06 11:27:45 +0100

    assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function App() {
-  return <p>GoBarber-Web</p>;
-}
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello Wolrd</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
