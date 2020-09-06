a038e3aa7fe5a4880a959eb728692a1a6ebade40
ruijadom <ruijadomingues@gmail.com>
2020-09-06 11:55:00 +0100

    style signin form

--- frontend/src/pages/Signin/styles.ts
+++ frontend/src/pages/Signin/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 import signInBackgroundImg from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
@@ -11,11 +12,51 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 
   place-content: center;
 
   width: 100%;
   max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 24px;
+  }
+
+  input {
+    background: #232129;
+    border-radius: 10px;
+    border: 2px solid #232129;
+    padding: 16px;
+    width: 100%;
+
+    & + input {
+      margin-top: 8px;
+    }
+  }
+
+  button {
+    background: #ff9000;
+    height: 56px;
+    border-radius: 10px;
+    border: 0;
+    padding: 16px;
+    color: #312e38;
+    width: 100%;
+    font-weight: 500;
+    margin-top: 16px;
+    transition: 0.2;
+
+    &:hover {
+      background: ${shade(0.2, '#ff9000')};
+    }
+  }
 `;
 
 export const Background = styled.div`
