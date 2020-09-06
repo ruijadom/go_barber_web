63806f22ba8facd5a7168920f6419edfce01bb14
ruijadom <ruijadomingues@gmail.com>
2020-09-06 11:47:34 +0100

    signin container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello Wolrd</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
--- frontend/src/pages/Signin/index.tsx
+++ frontend/src/pages/Signin/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
 
 const Signin: React.FC = () => (
-  <>
-    <h1>Signin</h1>
-  </>
+  <Container>
+    <Content>
+      <img src={logoImg} alt="Gobarber" />
+
+      <form>
+        <h1>Login</h1>
+
+        <input placeholder="E-mail" />
+
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Enter</button>
+
+        <a href="forgot">Forgot password</a>
+      </form>
+
+      <a href="">
+        <FiLogIn />
+        Create account
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
 );
 
 export default Signin;
