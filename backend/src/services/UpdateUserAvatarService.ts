847b08ea09659e4e39ac75ef4138c407aadbdc05
ruijadom <ruijadomingues@gmail.com>
2020-09-07 19:44:48 +0100

    backend

---
+++ backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar', 401);
+    }
+
+    if (user.avatar) {
+      const usersAvatarFilePath = path.join(
+        // Delete avatar before
+        uploadConfig.directory,
+        user.avatar,
+      );
+
+      const userAvatarFileExists = await fs.promises.stat(usersAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(usersAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
