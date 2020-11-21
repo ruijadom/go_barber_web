749e18c1a89ec06f9073ae0db1f0ec2a6f262605
ruijadom <ruijadomingues@gmail.com>
2020-11-21 20:44:11 +0000

    fix folders

---
+++ backend/dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: 'cb4f0d246dc8156e6c20d755123c4cd8',
+        expiresIn: '1d',
+    },
+};
